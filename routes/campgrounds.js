24e544b585c3c3bc3ce417e9a1d73d6f39e45d0f
Aashi <aashilahmad@gmail.com>
2023-07-23 23:05:21 -0400

    Yelp Camp

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,6 +4,7 @@ const catchAsync = require('../utils/catchAsync')
 const { campgroundSchema } = require('../schemas.js')
 const ExpressError = require('../utils/ExpressError');
 const Campground = require("../models/campground");
+const { isLoggedIn } = require('../middleware');
 
 const validateCampground = (req, res, next) => {
   
@@ -14,7 +15,7 @@ const validateCampground = (req, res, next) => {
   } else {
     next();
   }
-}
+ }
 
 
   router.get("/", catchAsync(async (req, res) => {
@@ -22,11 +23,11 @@ const validateCampground = (req, res, next) => {
     res.render("campgrounds/index", { campgrounds });
   }));
   
-  router.get("/new", (req, res) => {
+router.get("/new", isLoggedIn,(req, res) => {
     res.render("campgrounds/new");
   });
   
-router.post("/", validateCampground, catchAsync(async (req, res, next) => {
+router.post("/", isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
     // if (!req.body.campground) throw new ExpressError('Invalid Campground Data',400);
     
     const campground = new Campground(req.body.campground);
@@ -45,7 +46,7 @@ router.post("/", validateCampground, catchAsync(async (req, res, next) => {
     res.render("campgrounds/show", { campground });
   }));
   
-  router.get('/:id/edit', catchAsync(async (req, res) => {
+  router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     if (!campground) {
@@ -55,7 +56,7 @@ router.post("/", validateCampground, catchAsync(async (req, res, next) => {
     res.render("campgrounds/edit", { campground });
   }));
   
-  router.put('/:id', validateCampground, catchAsync(async (req, res) => {
+  router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     req.flash('success','Successfully updated campground!')
